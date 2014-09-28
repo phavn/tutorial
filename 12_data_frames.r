a6e9845391194c9394e662d764721a2ecf3bbc59
Pavel L <phavn@hotmail.com>
2014-09-27 21:48:07 -0400

    new file

---
+++ 12_data_frames.r
@@ -0,0 +1,37 @@
+x <- 10:1
+y <- -4:5
+q <- c("Hockey","Football","Baseball","Curling","Rugby","Lacrosse","Basketball","Tennis","Cricket","Soccer")
+
+theDF <- data.frame(x, y, q)
+theDF
+theDF <- data.frame(First=x, Second=y, Sport=q)
+class(theDF$Sport)
+
+theDF <- data.frame(First=x,Second=y,Sport=q,stringsAsFactors=FALSE)
+class(theDF$Sport)
+
+nrow(theDF)
+ncol(theDF)
+dim(theDF)
+names(theDF)
+names(theDF)[3]
+
+rownames(theDF)
+rownames(theDF) <- c("one","two","three","four","five","six","seven","eight","nine","ten")
+rownames(theDF)
+theDF
+rownames(theDF) <- NULL
+theDF
+
+head(theDF)
+head(theDF,n=9)
+
+tail(theDF)
+tail(theDF,n=8)
+class(theDF)
+
+theDF$Sport
+theDF[3, 2] # 3rd row, 2nd column
+theDF[3, 1:3]
+theDF[c(3,5),1:3]
+
