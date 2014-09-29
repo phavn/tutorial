2623331a10fc9aec2dc62e765952cf4f2f122074
Pavel L <phavn@hotmail.com>
2014-09-29 19:56:18 -0400

    mega changes

--- 23_scrape_from_web.r
+++ 23_scrape_from_web.r
@@ -1,3 +1,7 @@
 require(XML)
 
 theURL <- "http://www.jaredlander.com/2012/02/another-kind-of-super-bowl-pool/"
+
+bowlGame <- readHTMLTable(theURL, which=1, header=FALSE, stringsAsFactors=FALSE)
+bowlGame
+class(bowlGame)
