014434d2fe90356eeda1e13d353c9e5893f6a6dd
Pavel L <phavn@hotmail.com>
2014-09-29 11:35:56 -0400

    new files

---
+++ 22_load_r_packages_data.r
@@ -0,0 +1,9 @@
+require(ggplot2)
+
+data(diamonds)
+head(diamonds)
+
+data("tips", package="reshape2")
+head(tips)
+
+data()
---
+++ 23_scrape_from_web.r
@@ -0,0 +1,3 @@
+require(XML)
+
+theURL <- "http://www.jaredlander.com/2012/02/another-kind-of-super-bowl-pool/"
